8a295d41d0892d8b8e4f6185314e5a26d67fda43
antonysamy931 <antonysamy931@gmail.com>
2015-01-23 14:55:14 +0530

    image duplication avoid using checksum concept with SHA512 algorithm

---
+++ ImageCheckSum.POC/ImageCheckSum.Web/Controllers/HomeController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Web;
+using System.Web.Mvc;
+using System.IO.Compression;
+using ImageCheckSum.Web.Models;
+
+namespace ImageCheckSum.Web.Controllers
+{
+    public class HomeController : Controller
+    {
+        POCEntities entity = new POCEntities();
+        //
+        // GET: /Home/
+        [HttpGet]
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Index(HttpPostedFileBase image)
+        {            
+            
+            HashAlgorithm SHA512 = new SHA512Managed();
+            var startTime = DateTime.Now.ToLocalTime();
+            byte[] hash = SHA512.ComputeHash(image.InputStream);
+            var endTime = DateTime.Now.ToLocalTime();
+            var checksum = Convert.ToBase64String(hash.ToArray());
+            if (!entity.UploadChecksums.Any(x => x.filechecksum == checksum))
+            {
+                UploadChecksum uploadChecksum = new UploadChecksum();
+                uploadChecksum.filechecksum = checksum;
+                entity.UploadChecksums.Add(uploadChecksum);
+                entity.SaveChanges();
+            }            
+            return View();
+        }
+    }
+}
---
+++ ImageCheckSum.POC/ImageCheckSum.Web/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="ImageCheckSum.Web.MvcApplication" Language="C#" %>
